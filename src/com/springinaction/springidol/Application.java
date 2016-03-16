d387f24b337e3d2e4ef24bca10f2cfd82fc06f1e
Yagodka Andrey <yagodka.andrey@gmail.com>
2016-03-17 01:10:10 +0300

    initial commit

---
+++ src/com/springinaction/springidol/Application.java
@@ -0,0 +1,17 @@
+package com.springinaction.springidol;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+
+    public static void main(String[] args) throws PerformanceException {
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(
+                "com/springinaction/springidol/spring-idol.xml");
+        Performer performer = (Performer) ctx.getBean("duke");
+
+        performer.perform();
+
+    }
+}
